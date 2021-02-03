16b51befcaf941d7897e1d98b4cac574033073d2
Bilal Leadbitter <bilal.leadbitter@gmail.com>
2021-02-03 20:28:01 +0000

    Sorted list and wrote line to print first 5 items

--- Tech-Test.py
+++ Tech-Test.py
@@ -1,10 +1,28 @@
-import csv 
+import csv
 
 with open("Python Developer Test Dataset.csv") as file:
     reader = csv.reader(file)
-    for row in reader:
-        print(row)
+    next(reader)
+    sorted_numbers = sorted(reader, key=lambda row: float(row[-1]))
+
+    print(sorted_numbers[0:5])
+
+
+
+
+
+
+
+        
+
     
+
+
+    
+   
+
+    
+        
 
 
     
